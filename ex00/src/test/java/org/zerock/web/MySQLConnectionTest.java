05c1591f6afe4aa0ba227c62fabb2d4f31ed4b8d
jylee3349 <jwlee@servetech.co.kr>
2022-06-05 14:22:32 +0900

    save

--- ex00/src/test/java/org/zerock/web/MySQLConnectionTest.java
+++ ex00/src/test/java/org/zerock/web/MySQLConnectionTest.java
@@ -7,16 +7,22 @@ import org.junit.Test;
 
 public class MySQLConnectionTest {
 	
-	private static final String DRIVER = "com.mysql.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://39.118.52.71:3306/book_ex";
-	private static final String USER = "root";
-	private static final String PW = "!sgs@one.12";
+//	private static final String DRIVER = "com.mysql.jdbc.Driver";
+//	private static final String URL = "jdbc:mysql://39.118.52.71:3306/book_ex";
+//	private static final String USER = "root";
+//	private static final String PW = "!sgs@one.12";
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/book_ex";
+	private static final String USER = "jylee";
+	private static final String PW = "one.123";
 	
 	@Test
 	public void testConnection() throws Exception {
 		Class.forName(DRIVER);
 		
 		try(Connection con = DriverManager.getConnection(URL, USER, PW)) {
+//		try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost/book_ex?" +
+//                "user=jylee&password=one.123")) {
 			System.out.println(con);
 		} catch(Exception e) {
 			e.printStackTrace();
